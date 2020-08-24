a3fb1d8d3c738c2707e21cab013558bcf5db48ab
vasanthvels <vasanthkumarvel@gmail.com>
2020-08-24 19:47:56 +0530

    Integrated axios and changed the script to es6

--- server.js
+++ server.js
@@ -1,56 +1,42 @@
 /*Oxford Dictionary API - created by www.hybridappzone.com*/
-var express = require('express');
-var app = express();
-var request = require('request');
-var https = require('https');
-var rootCas = require('ssl-root-cas/latest').inject();
-https.globalAgent.options.ca = rootCas;
 
-app.get('/',function (req, res){
-  res.sendFile('views/index.html' , { root : __dirname});
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded())
+
+const instance = axios.create({
+  baseURL: 'https://od-api.oxforddictionaries.com',
+  headers: {
+    'Accept': 'application/json',
+    'app_id': process.env.APPID,
+    'app_key': process.env.APPKEY
+  }
 });
 
-app.post('/search',function (searchReq, searchRes){
-  var searchInput=""; var searchInputVal="";
-   searchReq.on('data', function (data) {
-      searchInput += data;
-      console.log("searchInput "+searchInput);
-      searchInput = searchInput.split("=");
-      searchInputVal = searchInput[1];
-
-  console.log("searchInputVal"+searchInputVal);
-  var postRequest = {
-      host: "od-api.oxforddictionaries.com",
-      path: "/api/v1/entries/en/"+searchInputVal,
-      port: "443",
-      method: "GET",
-      rejectUnauthorized: false,
-      headers: {
-          'Content-Type': 'text/html',
-          'app_id': process.env.APPID,
-          'app_key': process.env.APPKEY,
-          'Content-Length': Buffer.byteLength(searchInputVal)
-      }
-  };
-
-  var request = https.request(postRequest, function(response){
-    var searchData = "";
-    response.on( "data", function(data) { searchData = searchData + data;} );
-    response.on( "end", function(data) {
-      console.log("searchData"+ searchData);
-      searchRes.end(searchData);
-    });
-  });
-
-
-  request.on('error', function(error) {
-    console.log('problem with request: ' + error.message);
-  });
-
-  request.write(JSON.stringify(searchInputVal));
-  });
+app.get('/', function (req, res) {
+  res.sendFile('views/index.html', { root: __dirname });
 });
 
-var listener = app.listen(process.env.PORT, function () {
-    console.log('Your app is listening on port ' + listener.address().port);
+app.post('/search', (req, res) => {
+  const input = req.body.oxford;
+  try {
+    instance.get(`/api/v2/entries/en-us/${input}`)
+      .then(result => {
+        const data = {
+          "definition": result.data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0],
+          "phrases": result.data.results[0].lexicalEntries[0].phrases[0].text
+        }
+        res.status(200).send(data)
+      })
+      .catch(err => res.send(err));
+  }
+  catch (err) {
+    console.error(err);
+  }
+});
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
